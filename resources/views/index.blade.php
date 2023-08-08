6cfb63a91624884c7f6eb1462015fd8c8eedfbfc
Loctarogar <Morilon@protonmail.com>
2023-08-08 10:08:06 +0200

    games grid in index.blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-    index here
+    <div class="container mx-auto px-4">
+        <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
+        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800">
+            <div class="game mt-8">
+                Game goes here
+            </div>
+            <div class="game mt-8">
+                Game goes here
+            </div>
+            <div class="game mt-8">
+                Game goes here
+            </div>
+            <div class="game mt-8">
+                Game goes here
+            </div>
+            <div class="game mt-8">
+                Game goes here
+            </div>
+            <div class="game mt-8">
+                Game goes here
+            </div>
+            <div class="game mt-8">
+                Game goes here
+            </div>
+            <div class="game mt-8">
+                Game goes here
+            </div>
+            <div class="game mt-8">
+                Game goes here
+            </div>
+            <div class="game mt-8">
+                Game goes here
+            </div>
+            <div class="game mt-8">
+                Game goes here
+            </div>
+            <div class="game mt-8">
+                Game goes here
+            </div>
+
+        </div>
+
+    </div>
 @endsection
