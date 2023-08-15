cab3b0f8464ed31da1390fa7ad12e3ad604397df
Loctarogar <Morilon@protonmail.com>
2023-08-15 17:06:05 +0200

    single game page styling

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -64,6 +64,170 @@
             </div>
         </div>
 
+        <div class="images-container border-b border-gray-800 pb-12 mt-18">
+            <h2 class="h2 text-blue-500 uppercase tracking-wide font-semibold">Images</h2>
+            <div class="grid grid-cols-3 gap-12 mt-8">
+                <div class="">
+                    <a href="">
+                        <img src="/screenshot1.jpg" alt="screenshot" class="hover:opacity-75 transition ease-in-out">
+                    </a>
+                </div>
+                <div class="">
+                    <a href="">
+                        <img src="/screenshot1.jpg" alt="screenshot" class="hover:opacity-75 transition ease-in-out">
+                    </a>
+                </div>
+                <div class="">
+                    <a href="">
+                        <img src="/screenshot1.jpg" alt="screenshot" class="hover:opacity-75 transition ease-in-out">
+                    </a>
+                </div>
+                <div class="">
+                    <a href="">
+                        <img src="/screenshot1.jpg" alt="screenshot" class="hover:opacity-75 transition ease-in-out">
+                    </a>
+                </div>
+                <div class="">
+                    <a href="">
+                        <img src="/screenshot1.jpg" alt="screenshot" class="hover:opacity-75 transition ease-in-out">
+                    </a>
+                </div>
+                <div class="">
+                    <a href="">
+                        <img src="/screenshot1.jpg" alt="screenshot" class="hover:opacity-75 transition ease-in-out">
+                    </a>
+                </div>
+            </div>
 
+        </div>
+        <div class="similar-games-container  mt-18">
+            <h2 class="h2 text-blue-500 uppercase tracking-wide font-semibold">Similar Games</h2>
+            <div
+                class="
+            popular-games text-sm
+            grid
+             grid-cols-1
+             md:grid-cols-2
+             lg:grid-cols-5
+             xl:grid-cols-6
+             gap-12
+             "
+            >
+                <div class="game mt-8">
+                    <div class="relative inline-block">
+                        <a href="#">
+                            <img src="/ff7.jpg" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div
+                            class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                            style="right:-20px; bottom: -20px"
+                        >
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                80%
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" class="block text-base font-semibod leading-tight hover:text-gray-400 mt-8">
+                        Final Fantasy 7 Remake
+                    </a>
+                    <div class="text-gray-400 mt-1">Playstation 4</div>
+                </div>
+                <div class="game mt-8">
+                    <div class="relative inline-block">
+                        <a href="#">
+                            <img src="/ff7.jpg" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div
+                            class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                            style="right:-20px; bottom: -20px"
+                        >
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                80%
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" class="block text-base font-semibod leading-tight hover:text-gray-400 mt-8">
+                        Final Fantasy 7 Remake
+                    </a>
+                    <div class="text-gray-400 mt-1">Playstation 4</div>
+                </div>
+                <div class="game mt-8">
+                    <div class="relative inline-block">
+                        <a href="#">
+                            <img src="/ff7.jpg" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div
+                            class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                            style="right:-20px; bottom: -20px"
+                        >
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                80%
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" class="block text-base font-semibod leading-tight hover:text-gray-400 mt-8">
+                        Final Fantasy 7 Remake
+                    </a>
+                    <div class="text-gray-400 mt-1">Playstation 4</div>
+                </div>
+                <div class="game mt-8">
+                    <div class="relative inline-block">
+                        <a href="#">
+                            <img src="/ff7.jpg" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div
+                            class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                            style="right:-20px; bottom: -20px"
+                        >
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                80%
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" class="block text-base font-semibod leading-tight hover:text-gray-400 mt-8">
+                        Final Fantasy 7 Remake
+                    </a>
+                    <div class="text-gray-400 mt-1">Playstation 4</div>
+                </div>
+                <div class="game mt-8">
+                    <div class="relative inline-block">
+                        <a href="#">
+                            <img src="/ff7.jpg" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div
+                            class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                            style="right:-20px; bottom: -20px"
+                        >
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                80%
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" class="block text-base font-semibod leading-tight hover:text-gray-400 mt-8">
+                        Final Fantasy 7 Remake
+                    </a>
+                    <div class="text-gray-400 mt-1">Playstation 4</div>
+                </div>
+                <div class="game mt-8">
+                    <div class="relative inline-block">
+                        <a href="#">
+                            <img src="/ff7.jpg" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div
+                            class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                            style="right:-20px; bottom: -20px"
+                        >
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                80%
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" class="block text-base font-semibod leading-tight hover:text-gray-400 mt-8">
+                        Final Fantasy 7 Remake
+                    </a>
+                    <div class="text-gray-400 mt-1">Playstation 4</div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
